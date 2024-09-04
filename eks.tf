c92f93b78f201087ee3a02c55a744fd6311a2a11
Joel McCoy <joel@defenseunicorns.com>
2024-09-04 15:59:28 -0500

    try: public subnets

--- eks.tf
+++ eks.tf
@@ -5,12 +5,13 @@ module "eks-cluster" {
   cluster_version = "1.30"
 
   vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
 
   // Fargate profiles here
   fargate_profiles = {
     fargate_profile_1 = {
       name     = "fargate-profile"
-      subnets  = module.vpc.private_subnets
+      subnets  = module.vpc.public_subnets
       selectors = [
         {
           namespace = "default"
